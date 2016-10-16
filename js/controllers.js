6fdac6a7da64b0aa87a5b64531f7080a752ce433
Alias M6 <aliasmontoya719@gmail.com>
2016-10-16 17:09:46 -0600

    app has add to cart functionality working on updating quantity in cart

---
+++ js/controllers.js
@@ -0,0 +1,42 @@
+(function() {
+  'use strict';
+  const app = angular.module('cameraApp');
+  const camerasArray = [{id: 1,name: 'Nikon D3100 DSLR',image: 'http://ecx.images-amazon.com/images/I/713u2gDQqML._SX522_.jpg',rating: 4,price: 369.99,onSale: true},
+  {id: 2,name: 'Canon EOS 70D',image: 'http://ecx.images-amazon.com/images/I/81U00AkAUWL._SX522_.jpg',rating: 2,price: 1099.0,onSale: false},
+  {id: 3,name: 'Nikon D810A',image:'http://ecx.images-amazon.com/images/I/91wtXIfLl2L._SX522_.jpg', rating: 3, price: 3796.95, onSale: true}]
+
+  app.controller('CameraControl', function($rootScope) {
+    this.cameras = camerasArray;
+    $rootScope.shoppingCart = [];
+    $rootScope.shoppingCartprices = [];
+    let shoppingCart = $rootScope.shoppingCart;
+    let shoppingCartPrices = $rootScope.shoppingCartPrices;
+
+
+    this.addToCart = function(cam) {
+      shoppingCart.push(cam)
+      shoppingCartPrices = shoppingCart.map((e) => { return e.price });
+      $rootScope.shoppingCartSubTotal = shoppingCartPrices.reduce((current, previous) => {
+        return current + previous
+      });
+      $rootScope.shoppingCartTax = $rootScope.shoppingCartSubTotal * .0877899;
+      $rootScope.shoppingCartTotal = $rootScope.shoppingCartSubTotal + $rootScope.shoppingCartTax;
+    };
+  })
+  app.controller('CartControl', function($rootScope) {
+    let shoppingCartPrices = 0;
+
+
+
+    this.subTotal = function() {
+      if (shoppingCartPrices === 0) {
+        return 0;
+      } else {
+        return shoppingCartPrices.reduce((current, previous)=> {
+          return current + previous
+        })
+      }
+    }
+  })
+
+})();
